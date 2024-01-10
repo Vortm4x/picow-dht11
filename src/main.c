cfee15957f89ddb3daa07bc47c7a72fcc4cb8d83
Maksym Trehubenko <maksym.trehubenko@globallogic.com>
2024-01-10 07:28:10 +0200

    watchdog added

--- src/main.c
+++ src/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
-#include <hardware/gpio.h>
 #include <pico/cyw43_arch.h>
+#include <hardware/gpio.h>
+#include <hardware/watchdog.h>
 
 #include "dht11_driver.h"
 
 int main() 
-{   
+{
+    if(watchdog_caused_reboot())
+    {
+        printf("Timeout reached");
+    }
+
+    watchdog_enable(1100, true);
+
     const uint DHT_PIN = 15;
 
     stdio_init_all();
@@ -42,6 +50,8 @@ int main()
         {
             printf("INVALID DATA!\n");
         }
+
+        watchdog_update();
         
         sleep_ms(1000);
     }
